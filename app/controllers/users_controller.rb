7d68269db47af83f770e20fbcd7c90efd4bce046
Jaithoonnisha K <jaithoonnishak@Jaithoonnishas-MacBook-Air.local>
2025-09-03 01:38:53 +0530

    Fix UsersController & User model: auth, JSON messages, validations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :check_auth, only: [:show, :update, :destroy]
+  before_action :authenticate_user, only: [:show, :update, :destroy]
 
   # POST /signup
   def signup
@@ -22,44 +22,56 @@ class UsersController < ApplicationController
 
   # GET /users/:user_id
   def show
-    user = User.find_by(user_id: params[:user_id])
-    if user
+    if @current_user.user_id == params[:user_id]
       render json: {
-        message: "User information retrieved",
+        message: "User details by user_id",
         user: {
-          user_id: user.user_id,
-          nickname: user.nickname,
-          comment: user.comment
+          user_id: @current_user.user_id,
+          nickname: @current_user.nickname,
+          comment: @current_user.comment
         }
       }, status: :ok
     else
-      render json: { message: "User not found" }, status: :not_found
+      user = User.find_by(user_id: params[:user_id])
+      if user
+        render json: {
+          message: "User details by user_id",
+          user: {
+            user_id: user.user_id,
+            nickname: user.nickname,
+            comment: user.comment
+          }
+        }, status: :ok
+      else
+        render json: { message: "User not found" }, status: :not_found
+      end
     end
   end
 
   # PATCH /users/:user_id
   def update
-    user = User.find_by(user_id: params[:user_id])
-    if user&.update(update_params)
-      render json: { message: "Account successfully updated" }, status: :ok
+    if @current_user.user_id != params[:user_id]
+      render json: { message: "No permission for update" }, status: :forbidden
+      return
+    end
+
+    if @current_user.update(update_params)
+      render json: { message: "User successfully updated" }, status: :ok
     else
       render json: {
         message: "Account update failed",
-        cause: user&.errors&.full_messages&.first || "User not found"
+        cause: @current_user.errors.full_messages.first
       }, status: :bad_request
     end
   end
 
   # POST /close
   def destroy
-    user_id = params[:user_id] || params[:id]
-    user = User.find_by(user_id: user_id)
-
-    if user
-      user.destroy
+    if @current_user
+      @current_user.destroy
       render json: { message: "Account and user successfully removed" }, status: :ok
     else
-      render json: { message: "User not found" }, status: :not_found
+      render json: { message: "Authentication failed" }, status: :unauthorized
     end
   end
 
@@ -73,10 +85,15 @@ class UsersController < ApplicationController
     params.permit(:password, :nickname, :comment)
   end
 
-  def check_auth
-    auth = request.headers["Authorization"]
-    unless auth&.start_with?("Bearer")
-      render json: { message: "Unauthorized" }, status: :unauthorized
+  def authenticate_user
+    authenticate_or_request_with_http_basic do |user_id, password|
+      user = User.find_by(user_id: user_id)
+      if user&.password == password
+        @current_user = user
+      end
+    end
+    unless @current_user
+      render json: { message: "Authentication failed" }, status: :unauthorized
     end
   end
 end
