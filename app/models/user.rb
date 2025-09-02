2004a23ed660081ce181f993b19a6e3020587259
Jaithoonnisha K <jaithoonnishak@Jaithoonnishas-MacBook-Air.local>
2025-09-03 01:18:54 +0530

    Fix validation error messages for exam tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
   validates :user_id,
-            presence: true,
-            uniqueness: true,
-            length: { in: 6..20 },
+            presence: { message: "Required user_id and password" },
+            uniqueness: { message: "Already same user_id is used" },
+            length: { in: 6..20, message: "Input length is incorrect" },
             format: { with: /\A[a-zA-Z0-9]+\z/, message: "Incorrect character pattern" }
 
   validates :password,
-            presence: true,
-            length: { in: 8..20 },
+            presence: { message: "Required user_id and password" },
+            length: { in: 8..20, message: "Input length is incorrect" },
             format: { with: /\A[\x21-\x7E]+\z/, message: "Incorrect character pattern" }
 end
