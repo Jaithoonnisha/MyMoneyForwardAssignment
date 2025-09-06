84e7d7afb5dfa5f0c202d6b7f90688d5669051b0
Jaithoonnisha K <jaithoonnishak@Jaithoonnishas-MacBook-Air.local>
2025-09-06 16:15:40 +0530

    Update User model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,20 @@
 class User < ApplicationRecord
   validates :user_id,
-            presence: { message: "User Required user_id and password" },
             uniqueness: { message: "Already same user_id is used" },
             length: { in: 6..20, message: "Input length is incorrect" },
             format: { with: /\A[a-zA-Z0-9]+\z/, message: "Incorrect character pattern" }
 
   validates :password,
-            presence: { message: "User Required user_id and password" },
             length: { in: 8..20, message: "Input length is incorrect" },
             format: { with: /\A[\x21-\x7E]+\z/, message: "Incorrect character pattern" }
+
+  validate :user_id_and_password_required
+
+  private
+
+  def user_id_and_password_required
+    if user_id.blank? || password.blank?
+      errors.add(:base, "Required user_id and password")
+    end
+  end
 end
