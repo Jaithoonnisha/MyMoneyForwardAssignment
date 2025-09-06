57b943fb191ebaa86e4ef232c4dedcb06fabf56d
Jaithoonnisha K <jaithoonnishak@Jaithoonnishas-MacBook-Air.local>
2025-09-06 16:24:25 +0530

    Final UsersController with correct auth + messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,19 +21,19 @@ class UsersController < ApplicationController
 
   # GET /users/:user_id
   def show
-    user = authenticate_user(params[:user_id], params[:password])
-    unless user
-      return render json: { message: "Authentication failed" }, status: :unauthorized
+    user = User.find_by(user_id: params[:user_id])
+    if user
+      render json: {
+        message: "User information retrieved",
+        user: {
+          user_id: user.user_id,
+          nickname: user.nickname,
+          comment: user.comment
+        }
+      }, status: :ok
+    else
+      render json: { message: "User not found" }, status: :not_found
     end
-
-    render json: {
-      message: "User information retrieved",
-      user: {
-        user_id: user.user_id,
-        nickname: user.nickname,
-        comment: user.comment
-      }
-    }, status: :ok
   end
 
   # PATCH /users/:user_id
